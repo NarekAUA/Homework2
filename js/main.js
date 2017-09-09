a98aac245d28b337527accf0bcf468edd92bafbd
NarekAUA <narek_shamamyan@edu.aua.am>
2017-09-09 21:04:19 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+alert("My homework is done");
+
+//Checking whether AUA is an adult or not
+
+const birthYear = 1991;
+const presentYear = 2017;
+const ageAUA = presentYear - birthYear; 
+
+if (ageAUA >= 18) {
+	console.log('Yayyyy !!!');
+} else {
+	console.log('AUA is still a babyy :D');
+}
+//When will AUA be 50 years old? 
+const anniv50 = birthYear + 50;
+console.log('AUA will be 50 years old in ' + anniv50);
